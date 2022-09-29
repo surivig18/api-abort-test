a4428ad8bee1fecf6338133eef15c7f30c9205a1
surivig18 <surivig18@gmail.com>
2022-09-29 18:59:00 +0800

    Test abort

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let abortController = new AbortController()
+  const handleOnClick = async () => {
+    // setTimeout(()=> {
+    //   console.log("cancelled request ");
+    //   abortController.abort()}, 1000)
+      const response = await fetch
+      ('https://api.github.com/users/github',
+          {signal: abortController.signal}
+      );
+
+      const data = await response.json();
+      console.log(data);
+
+
+  }
+
+  const onCancelClick = (abort: AbortController) =>{
+    abort.abort()
+    abortController = new AbortController()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Submit onClick = {handleOnClick} abortController={abortController}/>
+      <Cancel onClick = {onCancelClick} abortController={abortController}/>
     </div>
   );
 }
 
+interface Props{
+  onClick: any
+  abortController: AbortController
+}
+
+const Submit = (props: Props) => {
+
+
+  return (
+      <button
+          style={{ width: '100px', height: '40px', backgroundColor: '  #4CAF50', margin: '10px' }}
+          onClick={props.onClick}
+      >
+        Submit API
+
+      </button>
+  )
+}
+
+const Cancel = (props: Props) => {
+
+  return (
+      <button
+          onClick={()=> props.onClick(props.abortController)}
+          style={{ width: '100px', height: '40px', backgroundColor: '  red', margin: '10px' }}>
+        Cancel API
+      </button>
+  )
+}
 export default App;
